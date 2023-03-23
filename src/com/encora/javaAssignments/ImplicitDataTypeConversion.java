96937252c6bd25b6d03deb113d28fed2faf299a1
Amruta Dhage <amruta.dhage@excellarate.com>
2023-03-23 13:38:30 +0530

    Program for Explicit, Implicit, Nonprimitive, Primitive Data Types conversions

---
+++ src/com/encora/javaAssignments/ImplicitDataTypeConversion.java
@@ -0,0 +1,20 @@
+package com.encora.javaAssignments;
+
+public class ImplicitDataTypeConversion 
+{
+
+	public static void main(String[] args) 
+	{
+		int a = 100;
+		System.out.println("Int representation: "+a);
+		
+		long b=a;
+		System.out.println("Long representation: "+b);
+		
+		float c = b;
+		System.out.println("Float representation: "+c);
+
+
+	}
+
+}
---
+++ src/com/encora/javaAssignments/PrimitiveDataTypes.java
@@ -0,0 +1,28 @@
+package com.encora.javaAssignments;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+	
+		// declaration of primitive data types
+		char a = 'A';
+		byte b = 2;
+		short c = 22;
+		int d = 45;
+		float e= 5.12345f;
+		double f = 4.6829d;
+		boolean g = true;
+		
+		System.out.println("char: " +a);
+		System.out.println("byte: " +b);
+		System.out.println("short: " +c);
+		System.out.println("int: " +d);
+		System.out.println("flaot: " +e);
+		System.out.println("double: " +f);
+		System.out.println("boolean: " +g);
+
+
+	}
+	
+
+}
