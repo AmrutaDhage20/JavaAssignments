e95570dff8cedfb1e5a01108956023b20af1bca1
Amruta Dhage <amruta.dhage@excellarate.com>
2023-03-23 16:46:57 +0530

    Array Programs

---
+++ src/com/encora/javaAssignments/ArrayForClass.java
@@ -0,0 +1,11 @@
+public class ArrayForClass {
+    public static void main (String[] args)
+    {
+        int[] arr = {10,20,30,50,60,80};
+
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ src/com/encora/javaAssignments/ArraysInJava.java
@@ -0,0 +1,13 @@
+public class ArraysInJava
+{
+    public static void main(String[] args) {
+        int[] arr = {11,22,33,44,55};
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+
+    }
+}
+
