f07c34697cc082b88d4a9805b03edfab179ec6a5
Amruta Dhage <amruta.dhage@excellarate.com>
2023-03-27 14:01:51 +0530

    Checkin all programs again

---
+++ src/com/encora/javaAssignments/TwoDimensionalArray.java
@@ -0,0 +1,12 @@
+public class TwoDimensionalArray {
+    public static void main(String[] args){
+        int arr[][]={{2, 7, 9}, {3, 6, 1}, {7,4,2}};
+
+        for (int i=0; i<3; i++){
+            for (int j=0; j<3; j++){
+                System.out.println(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
