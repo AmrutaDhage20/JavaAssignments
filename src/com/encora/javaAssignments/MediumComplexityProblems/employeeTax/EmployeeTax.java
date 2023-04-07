71edd7117141dc29ed214d31c45836b694ec85c5
Amruta Dhage <amruta.dhage@excellarate.com>
2023-04-07 15:45:27 +0530

    A file was deleted

--- src/com/encora/javaAssignments/MediumComplexityProblems/employeeTax/EmployeeTax.java
+++ src/com/encora/javaAssignments/MediumComplexityProblems/employeeTax/EmployeeTax.java
@@ -1,44 +1,43 @@
 package MediumComplexityProblems.employeeTax;
 
+import java.util.Scanner;
+
 public class EmployeeTax {
 
     /* Java program to calculate income tax */
 
-    public static void main(String args[]) {
+    public static void calculateIncomeTax(double salary) {
 
-        double income = 2000000, tax = 0;
         double taxableAmount = 0;
-        double taxFor10Percent = 0, taxFor15Percent = 0, taxFor20Percent = 0, taxFor30Percent = 0;
-        double totalTax;
+        int taxSlab = 0;
+        double tax = 0;
 
-        if (income <= 700000) {
-            tax = 0;
-        } else {
-            taxableAmount = income - 500000;
-            System.out.println("Total Taxable Amount:" + taxableAmount);
+        taxSlab = (salary > 700000 && salary <= 1000000) ? 2 : (salary > 1000000 && salary <= 1500000) ? 3 : (salary > 1500000) ? 4 : 1;
+        System.out.println("taxSlab: " + taxSlab);
 
-            taxFor10Percent = 0.1 * 200000;
-            System.out.println("taxFor10Percent: " + taxFor10Percent);
-            if (income <= 1000000) {
-                taxFor15Percent = (0.15 * (taxableAmount - 200000));
-                System.out.println("taxFor15Percent: " + taxFor15Percent);
-            }
-            if (income > 1000000 && income <= 1500000) {
-                taxFor15Percent = 0.15 * 300000;
-                taxFor20Percent = (0.20 * (taxableAmount - 300000 - 200000));
-                System.out.println("taxFor15Percent: " + taxFor15Percent);
-                System.out.println("taxFor20Percent: " + taxFor20Percent);
-            }
-            if (income > 1500000) {
-                taxFor15Percent = 0.15 * 300000;
-                taxFor20Percent = 0.20 * 500000;
-                taxFor30Percent = (0.30 * (taxableAmount - 300000 - 200000 - 500000));
-                System.out.println("taxFor15Percent: " + taxFor15Percent);
-                System.out.println("taxFor20Percent: " + taxFor20Percent);
-                System.out.println("taxFor30Percent: " + taxFor30Percent);
-            }
+        taxableAmount = salary - 500000;
+        System.out.println("Total Taxable Amount:" + taxableAmount);
+
+        switch (taxSlab) {
+
+            case 1:
+                System.out.println("Tax Free");
+                break;
+            case 2:
+                tax = (0.15 * (salary - 700000)) + (0.10 * 200000);
+                break;
+            case 3:
+                tax = (0.20 * (salary - 1000000)) + (0.15 * 300000) + (0.10 * 200000);
+                break;
+            case 4:
+                tax = (0.30 * (salary - 1500000)) + (0.20 * 500000) + (0.15 * 300000) + (0.10 * 200000);
+                break;
         }
-        totalTax = taxFor10Percent + taxFor15Percent + taxFor20Percent + taxFor30Percent;
-        System.out.println("Total IncomeTax is: " + totalTax);
+        System.out.println("Total IncomeTax is: " + (tax));
+    }
+
+    public static void main(String[] args) {
+
+        calculateIncomeTax(1200000);
     }
 }
