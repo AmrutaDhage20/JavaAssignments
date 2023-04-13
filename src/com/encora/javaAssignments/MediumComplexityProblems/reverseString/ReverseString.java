d57d60c95e71796160d584d67c86e4833330cb53
Amruta Dhage <amruta.dhage@excellarate.com>
2023-04-13 17:12:15 +0530

    Used void so it does not return anything

--- src/com/encora/javaAssignments/MediumComplexityProblems/reverseString/ReverseString.java
+++ src/com/encora/javaAssignments/MediumComplexityProblems/reverseString/ReverseString.java
@@ -2,7 +2,7 @@ package MediumComplexityProblems.reverseString;
 
 public class ReverseString {
     // Program to reverse the string without changing the position of special character
-    public static String stringReverse(String myString) {
+    public static void stringReverse(String myString) {
         StringBuilder str = new StringBuilder(myString);
         StringBuilder reverse = new StringBuilder();
         int position = -1;
@@ -11,16 +11,14 @@ public class ReverseString {
                 reverse.insert(0, str.charAt(i));
             } else {
                 position = i;
-                System.out.println(i);
             }
         }
         if (position != -1) {
             reverse.insert(position, '@');
         }
         System.out.println(reverse.toString());
-        return reverse.toString();
     }
     public static void main(String[] args) {
-        String myString = stringReverse("Ro@hitshetty");
+        stringReverse("Ro@hitshetty");
     }
 }
