527d08c1a24dd95ebd54e7deedefb2e26869e593
Amruta Dhage <amruta.dhage@excellarate.com>
2023-05-31 15:45:51 +0530

    Updated changes

--- src/com/encora/javaAssignments/DailyProgramPractice/program7/SearchGivenElement.java
+++ src/com/encora/javaAssignments/DailyProgramPractice/program7/SearchGivenElement.java
@@ -1,9 +1,9 @@
 package com.encora.javaAssignments.DailyProgramPractice.program7;
 
 public class SearchGivenElement {
-    public static int search(int array[], int firstNumber, int secondNumber) {
-        for (int index = 0; index < firstNumber; index++) {
-            if (array[index] == secondNumber)
+    public static int search(int array[], int firstArray, int secondArray) {
+        for (int index = 0; index < firstArray; index++) {
+            if (array[index] == secondArray)
                 return index;
         }
         return -1;
@@ -11,9 +11,9 @@ public class SearchGivenElement {
 
     public static void main(String[] args) {
         int[] array = {3, 4, 1, 7, 5};
-        int number = array.length;
-        int numberTwo = 4;
-        int index = search(array, number, numberTwo);
+        int arrayLength = array.length;
+        int number = 4;
+        int index = search(array, arrayLength, number);
         if (index == -1)
             System.out.println("Element is not present in the array");
         else
